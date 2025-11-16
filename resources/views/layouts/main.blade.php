1d10c7ac3fb85d26551cc020e780af20f23a2870
ALAN <alansatria110@gmail.com>
2025-11-17 00:59:53 +0700

    update home page

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KostHub</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+
+  @include('navbar.navbar')
+
+  <main>
+    @yield('content')
+  </main>
+
+  @include('navbar.footer')
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/navbar/navbar.blade.php
@@ -0,0 +1,10 @@
+ <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+
+<nav class="navbar" >
+  <img src="/img/logo_hitam.png" class="logo">
+  <div>
+    <a href="/register-pencari" class="btn masuk-btn rounded-pill px-4">Daftar</a>
+    <a href="/login" class="btn masuk-btn rounded-pill px-4">Masuk</a>
+</div>
+
+</nav>
